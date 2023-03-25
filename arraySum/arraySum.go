0ec41f19555db9b4b860e5e54d0b77f8d43d5486
Anderson Aguiar <andersonlaguiar@gmail.com>
2023-03-25 19:48:18 -0300

    feat: [M3S09] Ex 1 - Soma dos elementos de um array

---
+++ arraySum/arraySum.go
@@ -0,0 +1,11 @@
+package arraySum
+
+func ArraySum(arr []int)int{
+	sum := 0
+
+	for i := 0; i < len(arr); i++{
+		sum += arr[i]
+	}
+
+	return sum
+}
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"my_package/arraySum"
+)
+
 func main() {
+	myArray := []int{1,2,3,4}
+
+	var result int
+
+	result = arraySum.ArraySum(myArray)
+
+	fmt.Println(result)
 	
 }
